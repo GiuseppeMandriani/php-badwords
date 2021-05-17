c92869e257ec269f92d4636230f15af5d61df235
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-05-17 14:31:25 +0200

    start php

---
+++ php-badwords/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1>Welcome php, Esercizio 1</h1>
+
+
+    <!-- Creazione Variabile -->
+    <?php
+    // Dichiarazione Variabile
+    $paragraph = 'Suspendisse potenti. Donec sit amet lacus scelerisque, aliquet lacus nec, pulvinar dui. Curabitur eget porttitor diam, ut maximus nunc. Donec accumsan diam sit amet aliquet gravida. Aenean tincidunt diam in sagittis placerat. Morbi dolor lacus, suscipit vel elementum a, dignissim ac metus. Pellentesque dignissim convallis lectus, sed bibendum tortor tristique eget. Proin tortor tellus, condimentum eget bibendum eget, aliquam sit amet orci. Donec nec cursus orci, sed imperdiet nibh. Donec tincidunt lorem tortor, ut vulputate elit vestibulum non. Morbi hendrerit dictum tempor. Maecenas aliquam, mauris sit amet mollis tempus, tortor ipsum lobortis nibh, ac aliquet lectus augue at enim. Morbi posuere sapien sed purus pellentesque, id tempus urna mattis. Vestibulum imperdiet justo non orci tincidunt, sed sagittis sapien vulputate';
+
+
+    ?>
+
+
+
+
+
+
+
+    <!-- Esercizi Prove -->
+
+    <h2>Prove di php</h2>
+
+    <!-- Esempio Statico -->
+    <?php
+    #Creazione variabile
+    $name = 'Giuseppe Mandriani';
+    $age = 32;
+    $status = true;
+    ?>
+
+    <h3>Welcome, Sono <?php echo $name; ?> ho <?php echo $age; ?> anni</h3>
+
+
+
+
+
+
+
+
+</body>
+</html>
